d2c41fe8e7fd3e41b3b856311d365d5a6081f1fe
Jon Borgonia <jon.borgonia@gmail.com>
2020-07-27 16:35:53 -0700

    http, styles and demo data

--- http-polling/api.js
+++ http-polling/api.js
@@ -1,18 +1,38 @@
 const { Router } = require('express')
 const api = Router()
+const { agents, weapons } = require('./gameData')
 
 const eventDataSource = []
+
+// generate random game events
+const gameEvents = (function* (){
+
+  while(true){
+
+    const weaponIdx = Math.floor(Math.random() * weapons.length)
+    const sourcePlayerIdx = Math.floor(Math.random() * agents.length)
+    let targetPlayerIdx = Math.floor(Math.random() * agents.length)
+    while(sourcePlayerIdx === targetPlayerIdx) targetPlayerIdx = Math.floor(Math.random() * agents.length)
+    const type = Math.random() < .5 ? "KILL" : "DEATH"
+
+    yield {
+      id: Date.now(),
+      type,
+      sourcePlayer: agents[sourcePlayerIdx],
+      targetPlayer: agents[targetPlayerIdx],
+      weapon: weapons[weaponIdx],
+    }
+  }
+})()
 
 const queueGameEvent = () => {
   setTimeout(() => {
 
-    // simulate clear out the db every game
-    if(eventDataSource.length >= 20) eventDataSource.splice(0,eventDataSource.length)
+    // purge old events
+    if(eventDataSource.length >= 3) eventDataSource.splice(0,eventDataSource.length)
 
     // add new game event
-    eventDataSource.push({
-      id: Date.now(),
-    })
+    eventDataSource.push(gameEvents.next().value)
 
     queueGameEvent()
 
--- http-polling/static/js/app.js
+++ http-polling/static/js/app.js
@@ -1,24 +1,44 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { render } from 'react-dom'
 
 const POLL_INTERVAL = 2000
 
-const ActivityLog = ({ placeholder }) => <li>{placeholder}</li>
+const ActivityLog = ({ sourcePlayer, targetPlayer, type, weapon }) =>
+  <li className={`eventLog eventType-${type}`}>
+    <div className='sourcePlayer'>
+      <img className='avatar' src={`./images/${sourcePlayer.avatar}`} />
+      <span className='name'>{sourcePlayer.name}</span>
+      <img className='weapon' src={`./images/${weapon.graphic}`} />
+    </div>
+
+    <div className='targetPlayer'>
+      <span className='name'>{targetPlayer.name}</span>
+      <img className='avatar' src={`./images/${targetPlayer.avatar}`} />
+    </div>
+  </li>
 
 const App = () => {
-
   const [logs, setLogs] = useState([])
+  const timeout = useRef(null)
 
   useEffect(() => {
-    setInterval(() =>
+    timeout.current = setTimeout(() =>
       fetch('/api/game-events')
         .then(res => res.json())
         .then(setLogs)
-    ,POLL_INTERVAL)
-  }, [])
+    , POLL_INTERVAL)
+  }, [logs])
 
-  return <ul>
-    { logs.map(log => <ActivityLog key={log.id} placeholder={log.id} />) }
+  return <ul className='eventLogs'>
+    { logs.map(log =>
+      <ActivityLog
+        key={log.id}
+        sourcePlayer={log.sourcePlayer}
+        targetPlayer={log.targetPlayer}
+        type={log.type}
+        weapon={log.weapon}
+      />
+    )}
   </ul>
 }
 
